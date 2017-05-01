bd165dc53afe27d1f521442eac6ae508604928c1
Andrei Georgescu <ge0rgescu_andrei90@yahoo.com>
2017-05-01 11:15:49 +0300

    Revert "Revert "add constant medium and cornell box""
    This reverts commit ca532edb3011782651aa906e2e27a3104683e949.

--- data/raytracer/cornell_box.lua
+++ data/raytracer/cornell_box.lua
@@ -8,15 +8,15 @@ require "data.raytracer.raytracer_inc"
 function init()
 --	math.randomseed( os.time() )
 	math.randomseed( 9741 )
-	local light_intensity = 0.4
+	local light_intensity = 0.3
 
 --	local bgColour = vec3(0.3,0.3,0.3)
 	local bgColour = vec3(0, 0, 0)
 	local cameraPosition = vec3(0,0, 1.5)
 	local cameraLook = vec3(0,0,0)
---	local width = 190	local height = 1080	local samples = 1
-	local width = 1280	local height = 640	local samples = 1400
---	local width = 400	local height = 300	local samples = 1400
+--	local width = 1980	local height = 1080	local samples = 1
+--	local width = 1280	local height = 640	local samples = 150
+	local width = 400	local height = 300	local samples = 1000
 
 	InitFrame(vec3(width,height,samples), bgColour, cameraPosition, cameraLook)
 
@@ -32,7 +32,7 @@ function init()
 			 Colour = {r = light_intensity, g = light_intensity, b = light_intensity/2 },
 			 Position = {x = 0, y = 0.999, z =  0 },
 --			 Roughness = 0.32,
-			 Size = 0.25
+			 Size = 0.5
 			}
 
 -- ///////////////////////////////////////////////////////////////////////////
